032cee091621cfb209249d2cf1d18be9cfee3d7d
pione30 <g.pionelish30@gmail.com>
2018-01-31 00:16:33 +0900

    add humidity

--- weather2mackerel.py
+++ weather2mackerel.py
@@ -9,20 +9,28 @@ WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 MACKEREL_BASE_URL = 'https://api.mackerelio.com/api/v0/services/Weather/tsdb'
 
 weather_response = requests.get(
-        WEATHER_BASE_URL
-        + '?id=' + os.environ['WEATHER_CITY_ID']
-        + '&APPID=' + os.environ['WEATHER_API_KEY'])
+    WEATHER_BASE_URL
+    + '?id=' + os.environ['WEATHER_CITY_ID']
+    + '&APPID=' + os.environ['WEATHER_API_KEY']
+)
 
 headers = {
-        'X-Api-Key': os.environ["MACKEREL_API_KEY"],
-        'Content-Type': 'application/json'
-        }
+    'X-Api-Key': os.environ["MACKEREL_API_KEY"],
+    'Content-Type': 'application/json'
+}
 
-payload = [{
-        'name': 'pressure',
-        'time': time.time(),
-        'value': weather_response.json()['main']['pressure'] - 900
-        }]
+payload = []
+payload.append({
+    'name': 'pressure',
+    'time': time.time(),
+    'value': weather_response.json()['main']['pressure'] - 900
+})
+payload.append({
+    'name': 'percentage.humidity',
+    'time': time.time(),
+    'value': weather_response.json()['main']['humidity']
+})
 
 mackerel_response = requests.post(
-        MACKEREL_BASE_URL, headers=headers, data=json.dumps(payload))
+    MACKEREL_BASE_URL, headers=headers, data=json.dumps(payload)
+)
