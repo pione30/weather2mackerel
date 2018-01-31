e0613609d8d01c7733719b7ae7525b6b26ba2122
pione30 <g.pionelish30@gmail.com>
2018-02-01 01:14:21 +0900

    def main

--- weather2mackerel.py
+++ weather2mackerel.py
@@ -8,29 +8,36 @@ import time
 WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 MACKEREL_BASE_URL = 'https://api.mackerelio.com/api/v0/services/Weather/tsdb'
 
-weather_response = requests.get(
-    WEATHER_BASE_URL
-    + '?id=' + os.environ['WEATHER_CITY_ID']
-    + '&APPID=' + os.environ['WEATHER_API_KEY']
-)
 
-headers = {
-    'X-Api-Key': os.environ["MACKEREL_API_KEY"],
-    'Content-Type': 'application/json'
-}
+def main():
 
-payload = []
-payload.append({
-    'name': 'pressure',
-    'time': time.time(),
-    'value': weather_response.json()['main']['pressure'] - 900
-})
-payload.append({
-    'name': 'percentage.humidity',
-    'time': time.time(),
-    'value': weather_response.json()['main']['humidity']
-})
+    weather_response = requests.get(
+        WEATHER_BASE_URL
+        + '?id=' + os.environ['WEATHER_CITY_ID']
+        + '&APPID=' + os.environ['WEATHER_API_KEY']
+    )
 
-mackerel_response = requests.post(
-    MACKEREL_BASE_URL, headers=headers, data=json.dumps(payload)
-)
+    headers = {
+        'X-Api-Key': os.environ["MACKEREL_API_KEY"],
+        'Content-Type': 'application/json'
+    }
+
+    payload = []
+    payload.append({
+        'name': 'pressure',
+        'time': time.time(),
+        'value': weather_response.json()['main']['pressure'] - 900
+    })
+    payload.append({
+        'name': 'percentage.humidity',
+        'time': time.time(),
+        'value': weather_response.json()['main']['humidity']
+    })
+
+    requests.post(
+        MACKEREL_BASE_URL, headers=headers, data=json.dumps(payload)
+    )
+
+
+if __name__ == '__main__':
+    main()
